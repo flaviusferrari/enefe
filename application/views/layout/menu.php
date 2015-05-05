cdad2bf19ae266cf93cbc21bc220ad6d1be1737e
flavius <flavius@flavius-System-Product-Name>
2015-05-05 15:29:50 -0300

    NOvos menus

--- application/views/layout/menu.php
+++ application/views/layout/menu.php
@@ -14,28 +14,18 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <!-- Cadastros -->
+                <!-- ENEFES -->
                 <li class="dropdown">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Categorias<b class="caret"></b></a>
+                    <a class="dropdown-toggle" href="" data-toggle="dropdown">ENEFE<b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Jogos</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Tutoriais</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Vídeo Aulas</a></li>
+                        <li><a href="">2014</a></li>
+                        <li><a href="">2015</a></li>
                     </ul>
                 </li>
-                <!-- PRODUTOS -->
-                <li class="dropdown">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown">Produtos<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Jogos</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Tutoriais</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Vídeo Aulas</a></li>
-                    </ul>
-                </li>
+                <!-- INSCRIÇÃO -->
+                <li><a href="#">INSCRIÇÃO</a></li>
+                <!-- DOWLOADS -->
+                <li><a href="#">DOWNLOADS</a></li>
             </ul>            
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container -->
