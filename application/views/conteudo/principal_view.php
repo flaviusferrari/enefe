1c2518bb7a7c9af629b3e6168365bfc702b5fc63
flavius <flavius@flavius-System-Product-Name>
2015-05-05 16:20:09 -0300

    Ajustes no Layout das áreas em destaque da Home

--- application/views/conteudo/principal_view.php
+++ application/views/conteudo/principal_view.php
@@ -6,10 +6,11 @@
         <div class="col-md-4">
             <div class="widget">
                 <div class="widget-header">
-                    <h3>Objetivo</h3>
+                    <h3 class="text-center twhite">OBJETIVO</h3>
                 </div>
                 <div class="widget-content">
                     <p>Just widget as simple container with header and footer</p>
+                    <button type="button" class="btn btn-info btn-xs">Ler mais...</button>
                 </div>
             </div>
         </div>    
@@ -17,10 +18,11 @@
         <div class="col-md-4">
             <div class="widget">
                 <div class="widget-header">
-                    <h3>Enefe</h3>
+                    <h3 class="text-center twhite">ENEFE</h3>
                 </div>
                 <div class="widget-content">
                     <p>Just widget as simple container with header and footer</p>
+                    <button type="button" class="btn btn-info btn-xs">Ler mais...</button>
                 </div>
             </div>
         </div> 
@@ -28,10 +30,11 @@
         <div class="col-md-4">
             <div class="widget">
                 <div class="widget-header">
-                    <h3>Historia</h3>
+                    <h3 class="text-center twhite">HISTÓRIA</h3>
                 </div>
                 <div class="widget-content">
                     <p>Just widget as simple container with header and footer</p>
+                    <button type="button" class="btn btn-info btn-xs">Ler mais...</button>
                 </div>
             </div>
         </div>     
