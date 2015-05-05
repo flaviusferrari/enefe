f43f3b3c48a9500f77c44f2f7247b7714256ebc7
flavius <flavius@flavius-System-Product-Name>
2015-05-05 15:51:13 -0300

    Inserido uma área de acesso no cabeçalho

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -28,16 +28,24 @@
                         <img src="<?php echo base_url('includes/images/LogoCEERJmenu.png') ?>" alt="CEERJ">
                     </div>
 
-                    <!-- USUARIO -->
-                    <div id="login" class="col-md-8">
+                    <!-- DESCRIÇÃO -->
+                    <div class="col-md-8">
                         <h2>ENEFE - Encontro Estadual da Família Espírita</h2>
                         <p>O ENEFE agrupa em torno de si uma equipe de trabalhadores que o desenvolve durante todo o ano em várias 
                             regiões do Rio de Janeiro através dos Encontros de Família. Por esta razão, o encontro se tornou um marco 
                             na divulgação do trabalho junto às famílias.</p>
                     </div>
                     
-                    <!-- ESPAÇO -->
-                    <div id="login" class="col-md-8">
+                    <!-- LOGIN -->
+                    <div id="login" class="col-md-2">
+                        <div class="lbLogar">Logar</div>
+                        <form class="form" role="form">
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="" placeholder="Entre com seu email">
+                                <input type="password" class="form-control" id="" placeholder="Entre com a senha">
+                            </div>
+                            <button type="submit" class="btn btn-info btn-xs">Logar</button>
+                        </form>
                     </div>
                 </div>
             </div>
